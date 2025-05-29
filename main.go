43ccc8711b8e06d9f517669edd2d3a3daea0bba0
Vijay <vijendrasinghbhati2002@gmail.com>
2025-05-29 17:07:33 +0530

    basic Agentic OPS

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"google.golang.org/genai"
+)
+
+// type Response struct {
+// 	respType  int `type`
+// 	map[string]interface{}
+// }
+
+func main() {
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{
+		APIKey:  "AIzaSyCt4xwCe7rolx-7oPh7JOeOHPTT4IquifI",
+		Backend: genai.BackendGeminiAPI,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	BasePrompt := `You will be given some task to generate shell executable command for 
+	you eill be given user input and you need to generate a plan and create actions accordingly to complete the task, you can ask question in following format. you need to give response in josn format, no extra text needed.the format wiil be of following type ask 1 question per prompt.
+	return plain json string nothing else.
+
+					{'type':'typeofresponse', .... }
+
+					type can be of following 0->plan,
+											 1->action, 
+											 2->question,
+											 3->command.
+					
+					{'type':0,'plan': ['do this and this and this' , 'fllowing with this'] , 'req':[req1 , req2 , req3]} }}
+					{'type':1,'action': " "}
+					{'type':2,'question': "question statement here"}
+					{'type':3,'command':{"<give a shell executteable command in string here>"} }
+					`
+	history := []*genai.Content{
+		genai.NewContentFromText(BasePrompt, genai.RoleUser),
+	}
+
+	// var command string
+	// fmt.Scan(&command)
+	command := "create a k8s cluster with 2 master and 5 worker nodes ."
+	// history = append(history, genai.NewContentFromText(command, genai.RoleUser))
+	chat, _ := client.Chats.Create(ctx, "gemini-2.0-flash", nil, history)
+
+	var result map[string]interface{}
+
+	for i := 0; i < 50; i++ {
+
+		// fmt.Scan(&command)
+		res, _ := chat.SendMessage(ctx, genai.Part{Text: command})
+
+		if len(res.Candidates) > 0 {
+			text := res.Candidates[0].Content.Parts[0].Text
+			if strings.HasPrefix(text, "```json") {
+				text = strings.TrimPrefix(text, "```json")
+				text = strings.TrimSuffix(text, "```")
+				text = strings.TrimSpace(text)
+			}
+
+			fmt.Println(text)
+
+			err = json.Unmarshal([]byte(text), &result)
+			if err != nil {
+				fmt.Printf("errorr in unmarshaling ,%v\ns", err)
+			}
+
+			// fmt.Printf(result["type"])
+
+			// fmt.Println("impp")
+			if val, ok := result["type"].(float64); ok && int(val) == 2 {
+				reader := bufio.NewReader(os.Stdin)
+				fmt.Print("Enter text: ")
+				input, _ := reader.ReadString('\n')
+				command = strings.TrimSpace(input)
+				fmt.Printf("You entered: %s\n", input)
+			}
+			// fmt.Println("impp")
+
+		} else {
+			break
+		}
+
+	}
+}
